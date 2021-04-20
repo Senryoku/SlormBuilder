c9290bdac4b8ddec464f3cb130d88a5b11687d45
Senryoku <maretverdant@gmail.com>
2021-04-20 16:23:09 +0200

    Dunno

--- Extract.py
+++ Extract.py
@@ -2,9 +2,9 @@ import json
  
 with open("client/src/assets/data/dat_cla_0.json", encoding="utf8") as file:
     Skills = json.loads(file.read())
-    Mecanics = [s for s in Skills if s['TYPE'] == "mechanics"]
-    MecanicsDict = {}
-    for m in Mecanics:
-        MecanicsDict[m['EN_NAME']] = {k:v for (k,v) in m.items() if v != "" and v != None}
+    Mechanics = [s for s in Skills if s['TYPE'] == "mechanics"]
+    MechanicsDict = {}
+    for m in Mechanics:
+        MechanicsDict[m['EN_NAME']] = {k:v for (k,v) in m.items() if v != "" and v != None}
     with open("client/src/assets/data/mechanics.json", "w") as output:
-        json.dump(MecanicsDict, output)
+        json.dump(MechanicsDict, output)
