3184396355f3195ae079ad15511c74a328ea38ab
Senryoku <maretverdant@gmail.com>
2022-04-24 14:05:03 +0200

    Not needed in webpack 5 apparently

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,32 +1,9 @@
 module.exports = {
-	chainWebpack: (config) => {
-		config.module
-			.rule("images")
-			.use("url-loader")
-			.loader("url-loader")
-			.tap((options) => {
-				if(!options) options = {};
-				options.limit = 512;
-				return options;
-			});
-	},
 	configureWebpack: {
 		optimization: {
 			splitChunks: {
 				chunks: "all",
 			},
-		},
-		module: {
-			rules: [
-				{
-					test: /\.(jpg|png|gif|svg)$/,
-					loader: "image-webpack-loader",
-					// Specify enforce: 'pre' to apply the loader
-					// before url-loader/svg-url-loader
-					// and not duplicate it in rules with them
-					enforce: "pre",
-				},
-			],
 		},
 	},
 };
