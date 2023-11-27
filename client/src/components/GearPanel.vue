377407b7890e27b9c3abfc0d597be0dd46c31a87
Senryoku <maretverdant@gmail.com>
2023-11-27 01:35:43 +0100

    Fix character

--- client/src/components/GearPanel.vue
+++ client/src/components/GearPanel.vue
@@ -6,10 +6,18 @@
 				<slot v-for="t in ['helm', 'amulet']" :key="t" :name="t" />
 			</div>
 			<div class="left">
-				<slot v-for="t in ['shoulder', 'bracer', 'glove']" :key="t" :name="t" />
+				<slot
+					v-for="t in ['shoulder', 'bracer', 'glove']"
+					:key="t"
+					:name="t"
+				/>
 			</div>
 			<div class="right">
-				<slot v-for="t in ['body', 'cape', 'belt']" :key="t" :name="t" />
+				<slot
+					v-for="t in ['body', 'cape', 'belt']"
+					:key="t"
+					:name="t"
+				/>
 			</div>
 			<div class="rings">
 				<slot v-for="t in ['ring0', 'ring1']" :key="t" :name="t" />
@@ -28,162 +36,164 @@
 </template>
 
 <script>
-export default {
-	props: {
-		className: { type: String },
-	},
-};
+	export default {
+		props: {
+			className: { type: String },
+		},
+	};
 </script>
 
 <style scoped>
-.gear-panel {
-	position: relative;
-	width: 572px;
-	height: 840px;
-	background-image: url("../assets/extracted/sprites/spr_inventory_slot_panel/spr_inventory_slot_panel_0.png");
-	padding: 40px 10px 10px 10px;
-	box-sizing: border-box;
-}
+	.gear-panel {
+		position: relative;
+		width: 572px;
+		height: 840px;
+		background-image: url("../assets/extracted/sprites/spr_inventory_slot_panel/spr_inventory_slot_panel_0.png");
+		padding: 40px 10px 10px 10px;
+		box-sizing: border-box;
+	}
 
-.character {
-	position: absolute;
-	top: 205px;
-	left: 50%;
-	transform: translateX(calc(-50% + 8px)) scaleX(-1);
-	animation-duration: 1000ms;
-	animation-iteration-count: infinite;
-	animation-direction: forward;
-	width: 130px;
-	height: 146.25px;
-	background-size: cover;
-	image-rendering: crisp-edges; /* Firefox */
-	image-rendering: pixelated; /* Chrome */
-}
+	.character {
+		position: absolute;
+		top: 205px;
+		left: 50%;
+		transform: translateX(calc(-50%)) scaleX(-1);
+		animation-duration: 1000ms;
+		animation-iteration-count: infinite;
+		animation-direction: forward;
+		width: 130px;
+		height: 146.25px;
+		background-size: contain;
+		background-position: center;
+		background-repeat: no-repeat;
+		image-rendering: crisp-edges; /* Firefox */
+		image-rendering: pixelated; /* Chrome */
+	}
 
-.knight {
-	animation-name: idle-animation-knight;
-}
-.huntress {
-	animation-name: idle-animation-huntress;
-}
-.mage {
-	animation-name: idle-animation-mage;
-}
+	.knight {
+		animation-name: idle-animation-knight;
+	}
+	.huntress {
+		animation-name: idle-animation-huntress;
+	}
+	.mage {
+		animation-name: idle-animation-mage;
+	}
 
-@keyframes idle-animation-knight {
-	0% {
-		background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_0.png");
+	@keyframes idle-animation-knight {
+		0% {
+			background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_0.png");
+		}
+		25% {
+			background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_1.png");
+		}
+		50% {
+			background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_2.png");
+		}
+		75% {
+			background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_3.png");
+		}
+		100% {
+			background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_0.png");
+		}
 	}
-	25% {
-		background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_1.png");
-	}
-	50% {
-		background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_2.png");
-	}
-	75% {
-		background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_3.png");
-	}
-	100% {
-		background-image: url("../assets/extracted/sprites/spr_npc_knight_idle/spr_npc_knight_idle_0.png");
-	}
-}
 
-@keyframes idle-animation-huntress {
-	0% {
-		background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_0.png");
+	@keyframes idle-animation-huntress {
+		0% {
+			background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_0.png");
+		}
+		25% {
+			background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_1.png");
+		}
+		50% {
+			background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_2.png");
+		}
+		75% {
+			background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_3.png");
+		}
+		100% {
+			background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_0.png");
+		}
 	}
-	25% {
-		background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_1.png");
-	}
-	50% {
-		background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_2.png");
-	}
-	75% {
-		background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_3.png");
-	}
-	100% {
-		background-image: url("../assets/extracted/sprites/spr_npc_huntress_idle/spr_npc_huntress_idle_0.png");
-	}
-}
 
-@keyframes idle-animation-mage {
-	0% {
-		background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_0.png");
+	@keyframes idle-animation-mage {
+		0% {
+			background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_0.png");
+		}
+		25% {
+			background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_1.png");
+		}
+		50% {
+			background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_2.png");
+		}
+		75% {
+			background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_3.png");
+		}
+		100% {
+			background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_0.png");
+		}
 	}
-	25% {
-		background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_1.png");
+
+	.top {
+		display: flex;
+		position: absolute;
+		top: 70px;
+		left: 50%;
+		transform: translateX(-50%);
+		width: 170px;
+		justify-content: space-between;
 	}
-	50% {
-		background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_2.png");
+
+	.left,
+	.right {
+		display: flex;
+		flex-direction: column;
+		position: absolute;
+		top: 150px;
+		height: 230px;
+		justify-content: space-between;
 	}
-	75% {
-		background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_3.png");
+
+	.left {
+		left: 80px;
 	}
-	100% {
-		background-image: url("../assets/extracted/sprites/spr_npc_mage_idle/spr_npc_mage_idle_0.png");
+
+	.right {
+		right: 80px;
 	}
-}
 
-.top {
-	display: flex;
-	position: absolute;
-	top: 70px;
-	left: 50%;
-	transform: translateX(-50%);
-	width: 170px;
-	justify-content: space-between;
-}
+	.rings {
+		display: flex;
+		position: absolute;
+		top: 400px;
+		left: 50%;
+		transform: translateX(-50%);
+		width: 240px;
+		justify-content: space-between;
+	}
 
-.left,
-.right {
-	display: flex;
-	flex-direction: column;
-	position: absolute;
-	top: 150px;
-	height: 230px;
-	justify-content: space-between;
-}
+	.boot {
+		position: absolute;
+		top: 420px;
+		left: 50%;
+		transform: translateX(-50%);
+	}
 
-.left {
-	left: 80px;
-}
+	.v-separator {
+		position: absolute;
+		top: 520px;
+		left: 50%;
+		transform: translateX(-50%);
+	}
 
-.right {
-	right: 80px;
-}
-
-.rings {
-	display: flex;
-	position: absolute;
-	top: 400px;
-	left: 50%;
-	transform: translateX(-50%);
-	width: 240px;
-	justify-content: space-between;
-}
-
-.boot {
-	position: absolute;
-	top: 420px;
-	left: 50%;
-	transform: translateX(-50%);
-}
-
-.v-separator {
-	position: absolute;
-	top: 520px;
-	left: 50%;
-	transform: translateX(-50%);
-}
-
-.weapon {
-	position: absolute;
-	width: 420px;
-	height: 228px;
-	background-image: url("../assets/extracted/sprites/spr_weapon_box_inventory/spr_weapon_box_inventory_0.png");
-	bottom: 50px;
-	left: 50%;
-	transform: translateX(-50%);
-	text-align: center;
-}
+	.weapon {
+		position: absolute;
+		width: 420px;
+		height: 228px;
+		background-image: url("../assets/extracted/sprites/spr_weapon_box_inventory/spr_weapon_box_inventory_0.png");
+		bottom: 50px;
+		left: 50%;
+		transform: translateX(-50%);
+		text-align: center;
+	}
 </style>
