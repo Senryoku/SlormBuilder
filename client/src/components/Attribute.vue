f1a6ab892d97896b017d1f079daa838479ac7902
Senryoku <maretverdant@gmail.com>
2023-11-27 01:34:19 +0100

    Partially fix attribute styles

--- client/src/components/Attribute.vue
+++ client/src/components/Attribute.vue
@@ -19,120 +19,135 @@
 </template>
 
 <script>
-import { parseText } from "../utils.js";
+	import { parseText } from "../utils.js";
 
-export default {
-	name: "Attribute",
-	props: {
-		attr: { type: Object },
-	},
-	methods: {
-		n(s) {
-			return `<span class="number">${s}</span>`;
+	export default {
+		name: "Attribute",
+		props: {
+			attr: { type: Object },
 		},
-		p(s) {
-			return `<span class="small">${s}</span>`;
+		methods: {
+			n(s) {
+				return `<span class="number">${s}</span>`;
+			},
+			p(s) {
+				return `<span class="small">${s}</span>`;
+			},
 		},
-	},
-	computed: {
-		effects() {
-			let basics = {};
-			let others = [];
-			for (let e of this.attr.effects) {
-				if (
-					!e[this.settings.language + "_TEXT"] ||
-					e[this.settings.language + "_TEXT"] === ""
-				) {
-					let types = e.TYPE.split("|");
-					let stats = e.STAT.split("|");
-					for (let idx = 0; idx < stats.length; ++idx) {
-						let s = stats[idx];
-						if (!(s in basics))
-							basics[s] = { level: 75, value: 0, max: 0, type: types[idx] };
-						basics[s].level = Math.min(basics[s].level, e.LEVEL);
-						basics[s].value +=
-							e.ADDITIVE === null
-								? parseFloat(e.VALUE)
-								: parseFloat(e.VALUE) *
-								  Math.max(0, 1 + e.ADDITIVE * (this.attr.level - e.LEVEL));
-						basics[s].max +=
-							e.ADDITIVE === null
-								? parseFloat(e.VALUE)
-								: parseFloat(e.VALUE) *
-								  e.ADDITIVE *
-								  Math.max(0, 1 + 75 - e.LEVEL);
+		computed: {
+			effects() {
+				let basics = {};
+				let others = [];
+				for (let e of this.attr.effects) {
+					if (
+						!e[this.settings.language + "_TEXT"] ||
+						e[this.settings.language + "_TEXT"] === ""
+					) {
+						let types = e.TYPE.split("|");
+						let stats = e.STAT.split("|");
+						for (let idx = 0; idx < stats.length; ++idx) {
+							let s = stats[idx];
+							if (!(s in basics))
+								basics[s] = {
+									level: 75,
+									value: 0,
+									max: 0,
+									type: types[idx],
+								};
+							basics[s].level = Math.min(
+								basics[s].level,
+								e.LEVEL
+							);
+							basics[s].value +=
+								e.ADDITIVE === null
+									? parseFloat(e.VALUE)
+									: parseFloat(e.VALUE) *
+									  Math.max(
+											0,
+											1 +
+												e.ADDITIVE *
+													(this.attr.level - e.LEVEL)
+									  );
+							basics[s].max +=
+								e.ADDITIVE === null
+									? parseFloat(e.VALUE)
+									: parseFloat(e.VALUE) *
+									  e.ADDITIVE *
+									  Math.max(0, 1 + 75 - e.LEVEL);
+						}
+					} else {
+						others.push({
+							level: e.LEVEL,
+							text: parseText(e, this.settings.language, {
+								text: this.settings.language + "_TEXT",
+								value_base: "VALUE",
+								value_type: "TYPE",
+								value_stat: "STAT",
+							}),
+						});
 					}
-				} else {
-					others.push({
-						level: e.LEVEL,
-						text: parseText(e, this.settings.language, {
-							text: this.settings.language + "_TEXT",
-							value_base: "VALUE",
-							value_type: "TYPE",
-							value_stat: "STAT",
-						}),
+				}
+				let basicEffect = [];
+				for (let a in basics) {
+					basicEffect.push({
+						level: basics[a].level,
+						text: `+${this.n(basics[a].value)}${
+							basics[a].type
+						} ${this.p(
+							`(+${basics[a].max}${basics[a].type} Max.)`
+						)} ${this.translate(a)}`,
+						basic: true,
 					});
 				}
-			}
-			let basicEffect = [];
-			for (let a in basics) {
-				basicEffect.push({
-					level: basics[a].level,
-					text: `+${this.n(basics[a].value)}${basics[a].type} ${this.p(
-						`(+${basics[a].max}${basics[a].type} Max.)`
-					)} ${this.translate(a)}`,
-					basic: true,
-				});
-			}
-			basicEffect.sort((l, r) => l.level - r.level);
-			others.sort((l, r) => l.level - r.level);
-			return basicEffect.concat(others);
+				basicEffect.sort((l, r) => l.level - r.level);
+				others.sort((l, r) => l.level - r.level);
+				return basicEffect.concat(others);
+			},
 		},
-	},
-};
+	};
 </script>
 
 <style scoped>
-.attribute {
-	width: 406px;
-	text-align: center;
-}
+	.attribute {
+		width: 406px;
+		text-align: center;
+	}
 
-.attribute .top {
-	width: 406px;
-	height: 96px;
-	background-image: url("../assets/extracted/sprites/spr_item_tooltip_top/spr_item_tooltip_top_0.png");
-	background-position: center -56px;
-	padding-top: 21px;
-	line-height: 56px;
-	box-sizing: border-box;
-	font-size: 1.5rem;
-	color: var(--color);
-}
+	.attribute .top {
+		width: 406px;
+		height: 96px;
+		background-image: url("../assets/extracted/sprites/spr_item_tooltip_top/spr_item_tooltip_top_0.png");
+		background-position: center -56px;
+		padding-top: 21px;
+		line-height: 56px;
+		box-sizing: border-box;
+		font-size: 1.5rem;
+		color: var(--color);
+	}
 
-.attribute .body {
-	padding: 0 18px 24px 18px;
-	box-sizing: border-box;
-	background-image: url("../assets/extracted/sprites/spr_item_tooltip_bottom/spr_item_tooltip_bottom_0.png"),
-		url("../assets/extracted/sprites/spr_item_tooltip_repeat_1px/spr_item_tooltip_repeat_1px_0.png");
-	background-position: center bottom -20px, center;
-	background-repeat: no-repeat, repeat-y;
-}
+	.attribute .body {
+		padding: 0 18px 24px 18px;
+		box-sizing: border-box;
+		background-image: url("../assets/extracted/sprites/spr_item_tooltip_bottom/spr_item_tooltip_bottom_0.png"),
+			url("../assets/extracted/sprites/spr_item_tooltip_repeat_1px/spr_item_tooltip_repeat_1px_0.png");
+		background-position: center bottom -20px, center;
+		background-repeat: no-repeat, repeat-y;
+	}
 
-.non-activated {
-	color: #555;
-}
+	.non-activated {
+		color: #555;
+	}
 
-.non-basic {
-	margin: 0.75em;
-	white-space: pre-line;
-}
+	.non-basic {
+		margin: 0.75em;
+		white-space: pre-line;
+	}
 
-.attribute ::v-deep(.small) {
-	color: #555;
-}
+	.attribute ::v-deep(.small) {
+		color: #555;
+	}
 
-.attribute ::v-deep(.number) {
-	color: var(--color);
-}
+	.attribute ::v-deep(.number) {
+		color: var(--color);
+	}
 </style>
--- client/src/components/Attributes.vue
+++ client/src/components/Attributes.vue
@@ -20,7 +20,9 @@
 						"
 					/>
 					<img
-						v-for="e in effects[idx].filter((e) => e.ADDITIVE !== null)"
+						v-for="e in effects[idx].filter(
+							(e) => e.ADDITIVE !== null
+						)"
 						:key="e.REF"
 						class="effect-level"
 						:style="`left: ${76 + additiveEffectMargin(e.LEVEL)}px`"
@@ -29,7 +31,11 @@
 						"
 					/>
 					<div class="attr-indicators">
-						<div v-for="i in attributes[idx].level" :key="i" class="attr-point">
+						<div
+							v-for="i in attributes[idx].level"
+							:key="i"
+							class="attr-point"
+						>
 							<img :src="attrPointImage(idx, i)" />
 
 							<div class="attr-gain" />
@@ -40,12 +46,16 @@
 					<div
 						class="attr-button plus-button"
 						@click="plus($event, idx)"
-						:class="{ disabled: !editable || attributes[idx].level >= 75 }"
+						:class="{
+							disabled: !editable || attributes[idx].level >= 75,
+						}"
 					/>
 					<div
 						class="attr-button minus-button"
 						@click="minus($event, idx)"
-						:class="{ disabled: !editable || attributes[idx].level <= 0 }"
+						:class="{
+							disabled: !editable || attributes[idx].level <= 0,
+						}"
 					/>
 				</div>
 			</div>
@@ -57,328 +67,333 @@
 </template>
 
 <script>
-import { ref } from "vue";
-import { clamp } from "../utils.js";
-import AttributeData from "../assets/extracted/dat_att.json";
+	import { ref } from "vue";
+	import { clamp } from "../utils.js";
+	import AttributeData from "../assets/extracted/dat_att.json";
 
-import Tooltip from "./Tooltip.vue";
-import Attribute from "./Attribute.vue";
+	import Tooltip from "./Tooltip.vue";
+	import Attribute from "./Attribute.vue";
 
-export default {
-	name: "Attributes",
-	components: { Tooltip, Attribute },
-	props: {
-		values: { type: Array },
-		editable: { type: Boolean, default: true },
-	},
-	data() {
-		let effects = [];
-		for (let attr of AttributeData) {
-			while (attr.TRAIT > effects.length - 1) effects.push([]);
-			effects[attr.TRAIT].push(attr);
-		}
-		const attrNames = [
-			"Toughness",
-			"Savagery",
-			"Fury",
-			"Determination",
-			"Zeal",
-			"Willpower",
-			"Dexterity",
-			"Bravery",
-		];
-		const colors = [
-			"#2f5896",
-			"#a82f07",
-			"#7ab549",
-			"#a9852b",
-			"#3b9999",
-			"#4e2570",
-			"#a54e1e",
-			"#862d4c",
-		];
+	export default {
+		name: "Attributes",
+		components: { Tooltip, Attribute },
+		props: {
+			values: { type: Array },
+			editable: { type: Boolean, default: true },
+		},
+		data() {
+			let effects = [];
+			for (let attr of AttributeData) {
+				while (attr.TRAIT > effects.length - 1) effects.push([]);
+				effects[attr.TRAIT].push(attr);
+			}
+			const attrNames = [
+				"Toughness",
+				"Savagery",
+				"Fury",
+				"Determination",
+				"Zeal",
+				"Willpower",
+				"Dexterity",
+				"Bravery",
+			];
+			const colors = [
+				"#2f5896",
+				"#a82f07",
+				"#7ab549",
+				"#a9852b",
+				"#3b9999",
+				"#4e2570",
+				"#a54e1e",
+				"#862d4c",
+			];
 
-		let attributes = [];
-		for (let i = 0; i < attrNames.length; ++i) {
-			attributes.push({
-				name: attrNames[i],
-				effects: effects[i],
-				color: colors[i],
-				level: this.values ? this.values[i] : 0,
-			});
-		}
+			let attributes = [];
+			for (let i = 0; i < attrNames.length; ++i) {
+				attributes.push({
+					name: attrNames[i],
+					effects: effects[i],
+					color: colors[i],
+					level: this.values ? this.values[i] : 0,
+				});
+			}
 
-		return {
-			attributes,
-			attrNames,
-			effects,
-			tooltip: ref(null),
-			hoveredAttr: ref(attributes[0]),
-		};
-	},
-	mounted() {
-		// Preload animation
-		for (let i = 0; i < 8; ++i) {
-			const img = new Image();
-			img.src = require(`../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_${i}.png`);
-		}
-	},
-	methods: {
-		attrPointImage(idx, i) {
-			if (i > 0 && i % 15 === 0)
-				return require(`@/assets/extracted/sprites/spr_trait_point_losange/spr_trait_point_losange_${idx}.png`);
-			else if (i > 0 && i % 5 === 0)
-				return require(`@/assets/extracted/sprites/spr_trait_point_square/spr_trait_point_square_${idx}.png`);
-			else
-				return require(`@/assets/extracted/sprites/spr_trait_point_default/spr_trait_point_default_${idx}.png`);
+			return {
+				attributes,
+				attrNames,
+				effects,
+				tooltip: ref(null),
+				hoveredAttr: ref(attributes[0]),
+			};
 		},
-		additiveEffectMargin(i) {
-			--i;
-			let fifteen = Math.floor(i / 15);
-			let five = Math.floor(i / 5) - fifteen;
-			let r = 10 * (i - five - fifteen) + 16 * five + 16 * fifteen;
-			if (i > 0 && i % 15 === 0) r -= 12;
-			else if (i > 0 && i % 5 === 0) r -= 12;
-			return r;
+		mounted() {
+			// Preload animation
+			for (let i = 0; i < 8; ++i) {
+				const img = new Image();
+				img.src = require(`../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_${i}.png`);
+			}
 		},
-		serialize() {
-			return this.attributes.map((a) => a.level).join(",");
+		methods: {
+			attrPointImage(idx, i) {
+				if (i > 0 && i % 15 === 0)
+					return require(`@/assets/extracted/sprites/spr_trait_point_losange/spr_trait_point_losange_${idx}.png`);
+				else if (i > 0 && i % 5 === 0)
+					return require(`@/assets/extracted/sprites/spr_trait_point_square/spr_trait_point_square_${idx}.png`);
+				else
+					return require(`@/assets/extracted/sprites/spr_trait_point_default/spr_trait_point_default_${idx}.png`);
+			},
+			additiveEffectMargin(i) {
+				--i;
+				let fifteen = Math.floor(i / 15);
+				let five = Math.floor(i / 5) - fifteen;
+				let r = 10 * (i - five - fifteen) + 16 * five + 16 * fifteen;
+				if (i > 0 && i % 15 === 0) r -= 12;
+				else if (i > 0 && i % 5 === 0) r -= 12;
+				return r;
+			},
+			serialize() {
+				return this.attributes.map((a) => a.level).join(",");
+			},
+			importSave(values) {
+				for (let i = 0; i < this.attributes.length; ++i)
+					this.attributes[i].level = values[i];
+			},
+			displayTooltip(event, idx) {
+				this.hoveredAttr = this.attributes[idx];
+				this.$refs.tooltip.display(event);
+			},
+			plus(event, idx) {
+				if (!this.editable) return;
+				this.attributes[idx].level = clamp(
+					this.attributes[idx].level +
+						(event.getModifierState("Shift") ||
+						event.getModifierState("Alt")
+							? 10
+							: 1),
+					0,
+					75
+				);
+			},
+			minus(event, idx) {
+				if (!this.editable) return;
+				// contextmenu can't be triggered with getModifierState("Shift")
+				this.attributes[idx].level = clamp(
+					this.attributes[idx].level -
+						(event.getModifierState("Shift") ||
+						event.getModifierState("Alt")
+							? 10
+							: 1),
+					0,
+					75
+				);
+			},
 		},
-		importSave(values) {
-			for (let i = 0; i < this.attributes.length; ++i)
-				this.attributes[i].level = values[i];
-		},
-		displayTooltip(event, idx) {
-			this.hoveredAttr = this.attributes[idx];
-			this.$refs.tooltip.display(event);
-		},
-		plus(event, idx) {
-			if (!this.editable) return;
-			this.attributes[idx].level = clamp(
-				this.attributes[idx].level +
-					(event.getModifierState("Shift") || event.getModifierState("Alt")
-						? 10
-						: 1),
-				0,
-				75
-			);
-		},
-		minus(event, idx) {
-			if (!this.editable) return;
-			// contextmenu can't be triggered with getModifierState("Shift")
-			this.attributes[idx].level = clamp(
-				this.attributes[idx].level -
-					(event.getModifierState("Shift") || event.getModifierState("Alt")
-						? 10
-						: 1),
-				0,
-				75
-			);
-		},
-	},
-};
+	};
 </script>
 
 <style scoped>
-.attributes {
-	background-image: url("../assets/extracted/sprites/spr_inventory_trait_panel/spr_inventory_trait_panel_0.png");
-	text-align: center;
-	width: 1192px;
-	height: 840px;
-}
-
-.title {
-	padding-top: 40px;
-	margin-bottom: 100px;
-	height: 90px;
-	line-height: 90px;
-	font-size: 3rem;
-}
-
-.attr-bars {
-	margin: auto;
-	width: min-content;
-}
-
-.attr-bar-container {
-	display: flex;
-	align-items: center;
-}
-
-.attr-bar {
-	position: relative;
-	width: 924px;
-	height: 60px;
-	background-image: url("../assets/extracted/sprites/spr_trait_bar_large/spr_trait_bar_large_0.png");
-}
-
-.attr-bar-container:nth-child(odd) .attr-bar {
-	background-image: url("../assets/extracted/sprites/spr_trait_bar_large/spr_trait_bar_large_1.png");
-}
-
-.attr-icon {
-	position: absolute;
-	top: 12px;
-	left: 12.3px;
-	z-index: 2;
-}
-
-.attr-bar-container:nth-child(odd) .attr-icon {
-	left: 28px;
-}
-
-.attr-indicators {
-	display: flex;
-	position: absolute;
-	top: 18px;
-	left: 74px;
-	align-items: center;
-	height: 24px;
-}
-
-.attr-point {
-	position: relative;
-	margin: 0 2px;
-}
-
-.attr-point:nth-child(5n) {
-	margin: 6px;
-}
-
-.attr-point:nth-child(15n) {
-	margin: 4px 2px 0 2px;
-}
-
-.effect-level {
-	position: absolute;
-	top: 8px;
-}
-
-.attr-button-container {
-	background-image: url("../assets/extracted/sprites/spr_trait_button_back/spr_trait_button_back_0.png");
-	width: 100px;
-	height: 60px;
-	position: relative;
-}
-
-.attr-button {
-	width: 44px;
-	height: 44px;
-	cursor: pointer;
-}
-
-.attr-button.disabled {
-	pointer-events: none;
-}
-
-.attr-gain {
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	z-index: 1;
-
-	width: 75px;
-	height: 80px;
-
-	animation-duration: 400ms;
-	animation-iteration-count: 1;
-	animation-timing-function: linear;
-	animation-name: attr-gain-animation;
-	animation-direction: forward;
-}
-
-.attr-point:nth-child(15n) .attr-gain {
-	animation-name: attr-gain-major-animation;
-	width: 112.5px;
-	height: 120px;
-	background-size: cover;
-	background-repeat: no-repeat;
-}
-
-@keyframes attr-gain-animation {
-	0% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_0.png");
+	.attributes {
+		background-image: url("../assets/extracted/sprites/spr_inventory_trait_panel/spr_inventory_trait_panel_0.png");
+		text-align: center;
+		width: 1192px;
+		height: 840px;
 	}
-	14% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_1.png");
-	}
-	28% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_2.png");
-	}
-	42% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_3.png");
-	}
-	56% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_4.png");
-	}
-	70% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_5.png");
-	}
-	85% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_6.png");
-	}
-	100% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_7.png");
-	}
-}
 
-@keyframes attr-gain-major-animation {
-	0% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_0.png");
+	.title {
+		padding-top: 40px;
+		margin-bottom: 100px;
+		height: 90px;
+		line-height: 90px;
+		font-size: 3rem;
 	}
-	14% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_1.png");
-	}
-	28% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_2.png");
-	}
-	42% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_3.png");
-	}
-	56% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_4.png");
-	}
-	70% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_5.png");
-	}
-	85% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_6.png");
-	}
-	100% {
-		background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_7.png");
-	}
-}
 
-.plus-button {
-	position: absolute;
-	top: 8px;
-	left: 8px;
-	background-image: url("../assets/extracted/sprites/spr_trait_button_plus/spr_trait_button_plus_0.png");
-}
-.plus-button:hover {
-	background-image: url("../assets/extracted/sprites/spr_trait_button_plus/spr_trait_button_plus_1.png");
-}
-.plus-button:active {
-	background-image: url("../assets/extracted/sprites/spr_trait_button_plus/spr_trait_button_plus_2.png");
-}
-.plus-button.disabled {
-	background-image: url("../assets/extracted/sprites/spr_trait_button_plus/spr_trait_button_plus_4.png");
-}
+	.attr-bars {
+		margin: auto;
+		width: min-content;
+	}
 
-.minus-button {
-	position: absolute;
-	top: 8px;
-	right: 8px;
-	background-image: url("../assets/extracted/sprites/spr_trait_button_minus/spr_trait_button_minus_0.png");
-}
-.minus-button:hover {
-	background-image: url("../assets/extracted/sprites/spr_trait_button_minus/spr_trait_button_minus_1.png");
-}
-.minus-button:active {
-	background-image: url("../assets/extracted/sprites/spr_trait_button_minus/spr_trait_button_minus_2.png");
-}
-.minus-button.disabled {
-	background-image: url("../assets/extracted/sprites/spr_trait_button_minus/spr_trait_button_minus_4.png");
-}
+	.attr-bar-container {
+		display: flex;
+		align-items: center;
+	}
+
+	.attr-bar {
+		position: relative;
+		width: 924px;
+		height: 60px;
+		background-image: url("../assets/extracted/sprites/spr_trait_bar_large/spr_trait_bar_large_0.png");
+		background-repeat: no-repeat;
+	}
+
+	.attr-bar-container:nth-child(odd) .attr-bar {
+		background-image: url("../assets/extracted/sprites/spr_trait_bar_large/spr_trait_bar_large_1.png");
+		background-position: 16px;
+	}
+
+	.attr-icon {
+		position: absolute;
+		top: 12px;
+		left: 12.3px;
+		z-index: 2;
+	}
+
+	.attr-bar-container:nth-child(odd) .attr-icon {
+		left: 28px;
+	}
+
+	.attr-indicators {
+		display: flex;
+		position: absolute;
+		top: 18px;
+		left: 74px;
+		align-items: center;
+		height: 24px;
+	}
+
+	.attr-point {
+		position: relative;
+		margin: 0 2px;
+	}
+
+	.attr-point:nth-child(5n) {
+		margin: 6px;
+	}
+
+	.attr-point:nth-child(15n) {
+		margin: 4px 2px 0 2px;
+	}
+
+	.effect-level {
+		position: absolute;
+		top: 8px;
+	}
+
+	.attr-button-container {
+		background-image: url("../assets/extracted/sprites/spr_trait_button_back/spr_trait_button_back_0.png");
+		width: 100px;
+		height: 60px;
+		position: relative;
+	}
+
+	.attr-button {
+		width: 44px;
+		height: 44px;
+		cursor: pointer;
+	}
+
+	.attr-button.disabled {
+		pointer-events: none;
+	}
+
+	.attr-gain {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		z-index: 1;
+
+		width: 75px;
+		height: 80px;
+
+		animation-duration: 400ms;
+		animation-iteration-count: 1;
+		animation-timing-function: step-start;
+		animation-name: attr-gain-animation;
+		animation-direction: forward;
+	}
+
+	.attr-point:nth-child(15n) .attr-gain {
+		animation-name: attr-gain-major-animation;
+		width: 112.5px;
+		height: 120px;
+		background-size: contain;
+		background-repeat: no-repeat;
+		background-position: center;
+	}
+
+	@keyframes attr-gain-animation {
+		0% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_0.png");
+		}
+		14% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_1.png");
+		}
+		28% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_2.png");
+		}
+		42% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_3.png");
+		}
+		56% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_4.png");
+		}
+		70% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_5.png");
+		}
+		85% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_6.png");
+		}
+		100% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation_mini/spr_class_ui_animation_mini_7.png");
+		}
+	}
+
+	@keyframes attr-gain-major-animation {
+		0% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_0.png");
+		}
+		14% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_1.png");
+		}
+		28% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_2.png");
+		}
+		42% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_3.png");
+		}
+		56% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_4.png");
+		}
+		70% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_5.png");
+		}
+		85% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_6.png");
+		}
+		100% {
+			background-image: url("../assets/extracted/sprites/spr_class_ui_animation/spr_class_ui_animation_7.png");
+		}
+	}
+
+	.plus-button {
+		position: absolute;
+		top: 8px;
+		left: 8px;
+		background-image: url("../assets/extracted/sprites/spr_trait_button_plus/spr_trait_button_plus_0.png");
+	}
+	.plus-button:hover {
+		background-image: url("../assets/extracted/sprites/spr_trait_button_plus/spr_trait_button_plus_1.png");
+	}
+	.plus-button:active {
+		background-image: url("../assets/extracted/sprites/spr_trait_button_plus/spr_trait_button_plus_2.png");
+	}
+	.plus-button.disabled {
+		background-image: url("../assets/extracted/sprites/spr_trait_button_plus/spr_trait_button_plus_4.png");
+	}
+
+	.minus-button {
+		position: absolute;
+		top: 8px;
+		right: 8px;
+		background-image: url("../assets/extracted/sprites/spr_trait_button_minus/spr_trait_button_minus_0.png");
+	}
+	.minus-button:hover {
+		background-image: url("../assets/extracted/sprites/spr_trait_button_minus/spr_trait_button_minus_1.png");
+	}
+	.minus-button:active {
+		background-image: url("../assets/extracted/sprites/spr_trait_button_minus/spr_trait_button_minus_2.png");
+	}
+	.minus-button.disabled {
+		background-image: url("../assets/extracted/sprites/spr_trait_button_minus/spr_trait_button_minus_4.png");
+	}
 </style>
