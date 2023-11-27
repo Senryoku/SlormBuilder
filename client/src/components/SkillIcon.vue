7056e1de92f768d0e11e88f4afa04925dd7725c5
Senryoku <maretverdant@gmail.com>
2023-11-27 01:12:01 +0100

    Fix skill icon style

--- client/src/components/SkillIcon.vue
+++ client/src/components/SkillIcon.vue
@@ -18,97 +18,102 @@
 </template>
 
 <script>
-import { getSkillSprite } from "../utils.js";
-export default {
-	name: "SkillIcon",
-	props: {
-		className: { type: String },
-		skill: { type: Object },
-		selected: { type: Boolean },
-		support: { type: Boolean, default: false },
-		round: { type: Boolean, default: false },
-	},
-	data(props) {
-		let sprite = props.skill.image;
-		if (!sprite || props.support)
-			sprite = getSkillSprite(props.className, props.skill, props.support);
-		return {
-			image: sprite,
-		};
-	},
-};
+	import { getSkillSprite } from "../utils.js";
+	export default {
+		name: "SkillIcon",
+		props: {
+			className: { type: String },
+			skill: { type: Object },
+			selected: { type: Boolean },
+			support: { type: Boolean, default: false },
+			round: { type: Boolean, default: false },
+		},
+		data(props) {
+			let sprite = props.skill.image;
+			if (!sprite || props.support)
+				sprite = getSkillSprite(
+					props.className,
+					props.skill,
+					props.support
+				);
+			return {
+				image: sprite,
+			};
+		},
+	};
 </script>
 
 <style scoped>
-.skill {
-	position: relative;
-	margin: 6px;
-	z-index: 1;
-	-ms-user-select: none;
-	user-select: none;
-}
+	.skill {
+		position: relative;
+		margin: 6px;
+		z-index: 1;
+		-ms-user-select: none;
+		user-select: none;
+	}
 
-.skill-image {
-	width: 44px;
-	height: 44px;
+	.skill-image {
+		width: 44px;
+		height: 44px;
 
-	border-style: solid;
-	border-image-source: url("../assets/extracted/sprites/spr_borders/spr_borders_6.png");
-	border-image-slice: 12 12 12 12;
-	border-image-width: 12px;
-	border-image-outset: 0px 0px 0px 0px;
-	border-image-repeat: stretch stretch;
-	border-image-outset: 8px;
-	background-size: cover;
-	z-index: 2;
-}
+		border-style: solid;
+		border-image-source: url("../assets/extracted/sprites/spr_borders/spr_borders_6.png");
+		border-image-slice: 15 15 15 15;
+		border-image-width: 15px;
+		border-image-outset: 0px 0px 0px 0px;
+		border-image-repeat: stretch stretch;
+		border-image-outset: 6px;
+		background-size: cover;
+		z-index: 2;
+	}
 
-.skill-image.support {
-	width: 92px;
-}
+	.skill-image.support {
+		width: 92px;
+	}
 
-.selected {
-	border-image-source: url("../assets/extracted/sprites/spr_borders/spr_borders_2.png");
-}
+	.selected {
+		border-image-source: url("../assets/extracted/sprites/spr_borders/spr_borders_2.png");
+	}
 
-.selected.maxed:not(.passive) {
-	border-image-source: url("../assets/extracted/sprites/spr_borders/spr_borders_3.png");
-	border-image-slice: 16 16 16 16;
-	border-image-width: 16px;
-	border-image-outset: 8px;
-}
+	.selected.maxed:not(.passive) {
+		border-image-source: url("../assets/extracted/sprites/spr_borders/spr_borders_3.png");
+	}
 
-.passive {
-	border-image-slice: 22 22 22 22;
-	border-image-width: 22px;
-	border-image-outset: 0px 0px 0px 0px;
-	border-image-repeat: stretch stretch;
-	border-image-outset: 11px;
-	border-image-source: url("../assets/extracted/sprites/spr_border_round/spr_border_round_0.png");
-}
+	.passive {
+		border-image-slice: 18;
+		border-image-width: 18px;
+		border-image-outset: 0px 0px 0px 0px;
+		border-image-repeat: stretch stretch;
+		border-image-outset: 6px;
+		border-image-source: url("../assets/extracted/sprites/spr_border_round/spr_border_round_0.png");
+	}
 
-.passive img {
-	border-radius: 22px;
-}
+	.passive img {
+		border-radius: 18px;
+	}
 
-.passive.selected {
-	border-image-source: url("../assets/extracted/sprites/spr_border_round/spr_border_round_1.png");
-}
+	.passive.selected {
+		border-image-source: url("../assets/extracted/sprites/spr_border_round/spr_border_round_1.png");
+	}
 
-.passive.selected.maxed {
-	border-image-source: url("../assets/extracted/sprites/spr_border_round/spr_border_round_5.png");
-}
+	.passive.selected.maxed {
+		border-image-source: url("../assets/extracted/sprites/spr_border_round/spr_border_round_5.png");
+		border-image-slice: 20;
+		border-image-width: 20px;
+		border-image-outset: 0px 0px 0px 0px;
+		border-image-outset: 10px;
+	}
 
-.level {
-	position: absolute;
-	bottom: -11px;
-	right: -11px;
-	color: white;
-	background-image: url("../assets/extracted/sprites/spr_skill_rank/spr_skill_rank_0.png");
-	width: 22px;
-	height: 22px;
-	text-align: center;
-	padding-top: 2px;
-	box-sizing: border-box;
-}
+	.level {
+		position: absolute;
+		bottom: -11px;
+		right: -11px;
+		color: white;
+		background-image: url("../assets/extracted/sprites/spr_skill_rank/spr_skill_rank_0.png");
+		width: 22px;
+		height: 22px;
+		text-align: center;
+		padding-top: 2px;
+		box-sizing: border-box;
+	}
 </style>
