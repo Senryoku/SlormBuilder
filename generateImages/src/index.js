093a581e5206db38086b0a44015257335743af70
Senryoku <maretverdant@gmail.com>
2025-04-27 00:29:45 +0200

    Gen. reaper images

--- generateImages/src/index.js
+++ generateImages/src/index.js
@@ -3,28 +3,39 @@ import puppeteer from "puppeteer";
 async function generateImage() {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	
-	for(let weaponType of ["sword", "bow", "staff"]) {
-		for(let i = 0; i < 120; ++i) {
+
+	for (let weaponType of ["sword", "bow", "staff"]) {
+		for (let i = 0; i < 120; ++i) {
+			console.log(`Generating image for ${weaponType} #${i}...`);
 			try {
-				await page.goto(`http://localhost:8080/reaper/${weaponType}/${i}`);
-				await page.setViewport({width: 1920, height: 1080});
-				
-				
+				await page.goto(
+					`http://localhost:5173/SlormBuilder/reaper/${weaponType}/${i}`
+				);
+				await page.setViewport({ width: 1920, height: 1080 });
+
 				await page.evaluate(() => {
-					document.querySelector('body').style.backgroundColor = "transparent";
+					document.querySelector("body").style.backgroundColor =
+						"transparent";
 					//let header = document.querySelector('.header');
 					//header.parentNode.removeChild(header);
 				});
-				
-				await page.waitForSelector('.reaper');
-				const element = await page.$('.reaper');        // declare a variable with an ElementHandle
-				await element.screenshot({path: `./reapers/${weaponType}_${i}.png`, omitBackground: true	}); // take screenshot element in puppeteer
-				
-				console.log(`Saved image to ./reapers/${weaponType}_${i}.png`);
-				
+
+				await page.waitForSelector(".reaper");
+				const element = await page.$(".reaper"); // declare a variable with an ElementHandle
+				await element.screenshot({
+					path: `./reapers/${weaponType}_${i}.png`,
+					omitBackground: true,
+				}); // take screenshot element in puppeteer
+
+				console.log(
+					`  Saved image to ./reapers/${weaponType}_${i}.png`
+				);
+
 				//await page.screenshot({path : `./debug.png`});
-			} catch(e) {
+			} catch (e) {
+				console.error(
+					`Error generating image for ${weaponType} #${i}:`
+				);
 				console.error(e);
 			}
 		}
